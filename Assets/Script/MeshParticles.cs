a678b928172a606cd853591755893d9247e7426b
chocolife <noriyasu@gmail.com>
2017-05-11 06:42:09 +0900

    change particle script

--- Assets/Script/MeshParticles.cs
+++ Assets/Script/MeshParticles.cs
@@ -14,9 +14,9 @@ public class MeshParticles : MonoBehaviour {
 	private Vector3[] meshVertices;
 	// 各パーティクル情報
 	private ParticleSystem.Particle[] particles;
-
-
+	private Vector3 vertex_pos;
 	
+
 	void Start () {
 
 		meshVertices = TargetPrimitive.GetComponent<MeshFilter>().sharedMesh.vertices;
@@ -40,17 +40,29 @@ public class MeshParticles : MonoBehaviour {
 			// ちょっと遅延させてパーティクルを移動させる
 			//particles[i].position = particles[i].position * (1f - ParticleSpeed) + meshVertices[i] * ParticleSpeed;
 
-			particles[i].position = meshVertices[i];
-
+			//particles[i].position = meshVertices[i];
+			vertex_pos = meshVertices[i];
 
 			// 座標に応じてパーティクルをカラフルに
 			//particles[i].color = new Color(1f - meshVertices[i].x % 1f, 0.2f + meshVertices[i].y % 0.8f, 0.5f + meshVertices[i].z % 0.5f);
 
-			z_depth = Mathf.Abs(1 - particles[i].position.z);
+			z_depth = Mathf.Abs(1 - vertex_pos.z);
+
 			if (particles[i].position.y < 0) {
 				z_depth = 0.0f;
 			}
-			particles[i].color = new Color(0, 0.4f, 0.85f, z_depth);
+
+
+			float shift_y = (0.2f - Mathf.Abs(0.1f - vertex_pos.z)* 2);
+			particles[i].position = new Vector3(vertex_pos.x, vertex_pos.y + shift_y, vertex_pos.z);
+
+
+			if (particles[i].position.z > 0.1f) {
+				particles[i].color = new Color(0, 0.4f, 0.85f, z_depth);
+			} else {
+				particles[i].color = new Color(1f, 0, 0, z_depth);
+			}
+
 			particles[i].size = 0.025f;
 			
 			// ライフタイムを指定しないと表示されない（すぐ死んで消えてる？）
